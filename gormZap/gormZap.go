51c64f20f41e6ba4751b1aed254dd6d45c580504
tiandi <my.english.mail@qq.com>
2025-12-24 14:46:11 +0800

    新增慢sql

--- gormZap/gormZap.go
+++ gormZap/gormZap.go
@@ -2,11 +2,10 @@ package gormZap
 
 import (
 	"context"
+	"time"
+
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
-	"time"
-)
-import (
 	gormlogger "gorm.io/gorm/logger"
 )
 
@@ -19,27 +18,36 @@ import (
 */
 
 type GormZap struct {
-	ZapLogger *zap.Logger
-	LogLevel  gormlogger.LogLevel
+	ZapLogger     *zap.Logger
+	LogLevel      gormlogger.LogLevel
+	SlowThreshold time.Duration
 }
 
-func NewGormZap(newLogger *zap.Logger, logLevel gormlogger.LogLevel) GormZap {
+func NewGormZap(newLogger *zap.Logger, logLevel gormlogger.LogLevel, slowThreshold time.Duration) GormZap {
 	return GormZap{
-		ZapLogger: newLogger,
-		LogLevel:  logLevel,
+		ZapLogger:     newLogger,
+		LogLevel:      logLevel,
+		SlowThreshold: slowThreshold,
 	}
 }
 
 // 和原来的gormlooger一样使用，设置log的级别后返回新的log对象
 func (receiver GormZap) LogMode(level gormlogger.LogLevel) gormlogger.Interface {
 	return GormZap{
-		ZapLogger: receiver.ZapLogger,
-		LogLevel:  level,
+		ZapLogger:     receiver.ZapLogger,
+		LogLevel:      level,
+		SlowThreshold: receiver.SlowThreshold,
 	}
 }
 
 func (receiver GormZap) Info(ctx context.Context, str string, args ...interface{}) {
-	//判定现在指定的级别，当我们设置的级别是warn(3)，小于4，那么肯定打印不出来info信息
+	//gorm的logger等级过滤和zap相反
+	//  info是最大的，error是最小的
+	//  但是我们想要的是如果LogLevel设置的是info，那么debug无法打印
+	//  如果设置的是warn，那么info和debug无法打印
+	//  如果是设置的是error，那么warn,info,debug无法打印
+	//这里如果receiver.LogLevel < gormlogger.Info，那么可能是debug
+	//  那我们直接返回即可，否则就是打印info，因为这是info方法，是gorm调用的
 	if receiver.LogLevel < gormlogger.Info {
 		return
 	}
@@ -47,7 +55,6 @@ func (receiver GormZap) Info(ctx context.Context, str string, args ...interface{
 	receiver.ZapLogger.Sugar().Info(str, args)
 }
 func (receiver GormZap) Warn(ctx context.Context, str string, args ...interface{}) {
-	//当我们设置的级别是error(2)，小于3(warn)，那么肯定打印不出来warn的信息
 	if receiver.LogLevel < gormlogger.Warn {
 		return
 	}
@@ -76,7 +83,10 @@ func (receiver GormZap) Trace(ctx context.Context, begin time.Time, fc func() (s
 			zap.Error(err),
 		}
 		receiver.ZapLogger.Error("", fields...)
-	case receiver.LogLevel >= gormlogger.Warn:
+	//其他情况我们一般打印info
+	//  但是我们的等级必须大于等于info才可以，如果是warn，那么就不应该打印info了
+	//  同时你的慢sql时间要大于实际时间，这样才能打印info，不然就是慢日志了，那样就要打印warn了
+	case receiver.LogLevel >= gormlogger.Info && receiver.SlowThreshold > latency:
 		sql, rowsAffected := fc()
 		fields := []zapcore.Field{
 			zap.String("sql", sql),
@@ -84,13 +94,15 @@ func (receiver GormZap) Trace(ctx context.Context, begin time.Time, fc func() (s
 			zap.Duration("latency", latency),
 		}
 		receiver.ZapLogger.Info("", fields...)
-	case receiver.LogLevel >= gormlogger.Info:
+	//然后是打印慢sql，要用warn，但是我们的等级必须要大于warn可行
+	case receiver.LogLevel >= gormlogger.Warn && receiver.SlowThreshold <= latency:
 		sql, rowsAffected := fc()
 		fields := []zapcore.Field{
 			zap.String("sql", sql),
 			zap.Int64("rowsAffected", rowsAffected),
 			zap.Duration("latency", latency),
 		}
-		receiver.ZapLogger.Info("", fields...)
+		receiver.ZapLogger.Warn("slowSql", fields...)
 	}
+
 }
